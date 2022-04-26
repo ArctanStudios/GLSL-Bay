76aa8ab2ea281692ace018dbe2f5157991d9ca2a
Sir Code <taylorschneider07@yahoo.com>
2022-04-26 19:41:58 -0400

    Drawing triangles

--- Main.cpp
+++ Main.cpp
@@ -2,18 +2,17 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
-const char* vertexShaderSource =
-"#version 337 core\n"
-"layout (location = 0) in vec3 aPos;"
+const char* vertexShaderSource = 
+"#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
 "void main() {\n"
-"gl_Position = Vec4(aPos.x, aPos.y, aPos.z, 1.0);"
+"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 "}\0";
-
 const char* fragmentShaderSource =
-"#version 337 core\n"
-"out vec4 FragColor"
+"#version 330 core\n"
+"out vec4 FragColor;\n"
 "void main() {\n"
-"FragColor = Vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
+"FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
 "}\0";
 
 int main() {
@@ -70,6 +69,12 @@ int main() {
 	glfwSwapBuffers(window);
 
 	while (!glfwWindowShouldClose(window)) {
+		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
