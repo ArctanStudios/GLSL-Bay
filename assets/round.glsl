f182180d6d26646faa9826ff630ed64099afa011
Sir Code <taylorschneider07@yahoo.com>
2022-04-29 20:34:22 -0400

    3D mandelbulb :D

--- assets/round.glsl
+++ assets/round.glsl
@@ -6,10 +6,10 @@ uniform vec3 iResolution;
 uniform vec4 iMouse;
 uniform float iTime;
 
-const int MAX_MARCHING_STEPS = 255;
+const int MAX_MARCHING_STEPS = 512;
 const float MIN_DIST = 0.0;
-const float MAX_DIST = 100.0;
-const float EPSILON = 0.0001;
+const float MAX_DIST = 200.0;
+const float EPSILON = 0.00001;
 
 const int aoIter = 8;
 const float aoDist = 0.07;
@@ -61,13 +61,14 @@ mat3 rotateZ(float theta) {
 }
 
 float sdMandelbrot(vec2 p) {
+    p.x /= 2.0;
     vec2 c = p;
     vec2 z = vec2(0.0,0.0);
     vec2 dz = vec2(0.0,0.0);
     bool exterior = true;
     float r2;
     float n = 0.0;
-    for(int i = 0; i<256; i++) {
+    for(int i = 0; i<64; i++) {
         // dz -> 2·z·dz + 1
         dz = 2.0*vec2(z.x*dz.x - z.y*dz.y, z.x*dz.y + z.y*dz.x) + vec2(1.0,0.0);
         // z -> z² + c
@@ -85,6 +86,50 @@ float sdMandelbrot(vec2 p) {
     return (exterior) ? d : 0.0;
 }
 
+vec3 mb(vec3 p, float power) {
+	p.xyz = p.xzy;
+	vec3 z = p;
+	vec3 dz=vec3(0.0);
+	float r, theta, phi;
+	float dr = 1.0;
+	
+	float t0 = 1.0;
+	for(int i = 0; i < 7; ++i) {
+		r = length(z);
+		if(r > 2.0) continue;
+		theta = atan(z.y / z.x);
+        #ifdef phase_shift_on
+		phi = asin(z.z / r) + iTime*0.1;
+        #else
+        phi = asin(z.z / r);
+        #endif
+		
+		dr = pow(r, power - 1.0) * dr * power + 1.0;
+	
+		r = pow(r, power);
+		theta = theta * power;
+		phi = phi * power;
+		
+		z = r * vec3(cos(theta)*cos(phi), sin(theta)*cos(phi), sin(phi)) + p;
+		
+		t0 = min(t0, r);
+	}
+	return vec3(0.5 * log(r) * r / dr, t0, 0.0);
+}
+
+void ry(inout vec3 p, float a) {  
+ 	float c,s;
+    vec3 q = p;  
+  	c = cos(a); s = sin(a);  
+  	p.x = c * q.x + s * q.z;  
+  	p.z = -s * q.x + c * q.z; 
+}
+
+vec3 f(vec3 p, float power){ 
+	ry(p, iTime / 4.0);
+    return mb(p, power); 
+}
+
 float opExtrussion(vec3 p, float sdf, float h) {
     vec2 w = vec2(sdf, abs(p.z) - h);
   	return min(max(w.x,w.y),0.0) + length(max(w,0.0));
@@ -92,13 +137,19 @@ float opExtrussion(vec3 p, float sdf, float h) {
 
 
 vec2 opRevolution(vec3 p, float w) {
-    return vec2( length(p.yz) - w, p.x );
+    return vec2(length(p.yz) - w, p.x );
 }
 
 float sceneSDF(vec3 samplePoint) {
     // return sdMandelbrot(opRevolution(samplePoint, 0.5));
-    samplePoint = rotateY(iTime / 2.0) * samplePoint;
-    return sdMandelbrot(opRevolution(samplePoint, 1.0));
+    samplePoint *= rotateY(iTime / 2.0);
+    samplePoint *= rotateX(sin(iTime / 5.0));
+    // return sdMandelbrot(opRevolution(samplePoint, 1.0)) - 0.01;
+    return f(samplePoint/1.5, 4.0).x - 0.001;
+}
+
+vec3 opRep(vec3 p, vec3 c) {
+    return mod(p + 0.5 * c, c) - 0.5 * c;
 }
 
 float shortestDistanceToSurface(vec3 eye, vec3 marchingDirection, float start, float end) {
@@ -137,17 +188,15 @@ mat3 viewMatrix(vec3 eye, vec3 center, vec3 up) {
     return mat3(s, u, -f);
 }
 
-float calcAO(vec3 pos, vec3 nor) {
-	float occ = 0.0;
-    float sca = 1.0;
-    for(int i = 0; i < 5; i++) {
-        float h = 0.01 + 0.12 * float(i) / 4.0;
-        float d = sceneSDF(pos + h * nor);
-        occ += (h - d) * sca;
-        sca *= 0.95;
-        if(occ > 0.35) break;
+float ao(vec3 p, vec3 n) {
+    float dist = aoDist;
+    float occ = 1.0;
+    for (int i = 0; i < aoIter; ++i) {
+        occ = min(occ, sceneSDF(p + dist * n) / dist);
+        dist *= aoPower;
     }
-    return clamp( 1.0 - 3.0*occ, 0.0, 1.0 ) * (0.5+0.5*nor.y);
+    occ = max(occ, 0.0);
+    return occ;
 }
 
 void main() {
@@ -162,5 +211,6 @@ void main() {
     }
     vec3 pos = eye + dist * worldDir;
     vec3 normals = estimateNormal(pos);
-    FragColor = vec4(normals, 1.0);
+    float ao = ao(pos, normals);
+    FragColor = vec4(normals * ao * normals * 5.0, 1.0);
 }
